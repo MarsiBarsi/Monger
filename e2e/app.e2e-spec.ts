737a265cc9a96a0849df0bf950b578af7fc0e2f4
Roman Sedov <darragon-nn@yandex.ru>
2018-04-03 08:30:51 +0300

    design

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+import { PdFreeAngularcliPage } from './app.po';
 
-describe('monger-app App', () => {
-  let page: AppPage;
+describe('pd-free-angularcli App', () => {
+  let page: PdFreeAngularcliPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new PdFreeAngularcliPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
   });
 });
